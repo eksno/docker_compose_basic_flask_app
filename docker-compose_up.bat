7f6ce9c81efc4c5f1469c0fd953f7280dad3b181
Jonas. L <33558637+Eksno@users.noreply.github.com>
2021-11-25 12:35:28 +0100

    convert to docker-compose

--- docker-compose_up.bat
+++ docker-compose_up.bat
@@ -1,11 +1,18 @@
 @ECHO OFF
 
+
 Rem Get name of folder program is running in
 set MYDIR=%~dp0
 set MYDIR1=%MYDIR:~0,-1%
 
 for %%f in (%MYDIR1%) do set myfolder=%%~nxf
 
-docker exec -it %myfolder% /bin/bash
+
+echo.
+echo Running %myfolder% using docker_compose:
+
+echo - Running using docker_compose...
+docker-compose up
+
 
 PAUSE
